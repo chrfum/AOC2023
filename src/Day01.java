7e00de4df86de8ecf93af5935c54ba5fac1cfa99
Christian Fumagalli <73828171+chrfum@users.noreply.github.com>
2023-12-04 12:49:46 +0100

    day01

--- src/Day01.java
+++ src/Day01.java
@@ -7,12 +7,12 @@ public class Day01 extends AOCDay {
         for (String line : input) {
             String ints = "";
             line = line.replaceAll("one", "o1e");
-            line = line.replaceAll("two", "t2");
-            line = line.replaceAll("three", "t3");
-            line = line.replaceAll("four", "4");
-            line = line.replaceAll("five", "5e");
-            line = line.replaceAll("six", "6");
-            line = line.replaceAll("seven", "7n");
+            line = line.replaceAll("two", "t2o");
+            line = line.replaceAll("three", "t3e");
+            line = line.replaceAll("four", "f4r");
+            line = line.replaceAll("five", "f5e");
+            line = line.replaceAll("six", "s6x");
+            line = line.replaceAll("seven", "s7n");
             line = line.replaceAll("eight", "e8t");
             line = line.replaceAll("nine", "n9e");
             for (Character c : line.toCharArray()) {
