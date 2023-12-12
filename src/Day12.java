b23d24bfd6be3d27d82495220effcd46d790c916
Christian Fumagalli <73828171+chrfum@users.noreply.github.com>
2023-12-12 15:44:12 +0100

    day12

--- src/Day12.java
+++ src/Day12.java
@@ -1,4 +1,8 @@
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.StringJoiner;
 
@@ -6,68 +10,72 @@ public class Day12 extends AOCDay {
     
     public void solve(File in) {
         inputAsList(in);
-        long res = 0;
-        
+        long res1 = 0;
+        long res2 = 0;
+
         for (String line : input) {
             Scanner sc = new Scanner(line);
             String s = sc.next();
             String n = sc.next();
-          
-            long x = permute(s, n, 0);
             
-            res += x;
             
+            StringJoiner sj1 = new StringJoiner("?");
+            StringJoiner sj2 = new StringJoiner(",");
+            for (int i = 0; i < 5; i++) {
+                sj1.add(s);
+                sj2.add(n);
+            }
+            String s2 = sj1.toString();
+            String n2 = sj2.toString();
+            
+            
+            String[] d = n.split(",");
+            int[] damaged = new int[d.length];
+            for (int i = 0; i < d.length; i++) damaged[i] = Integer.parseInt(d[i]);
+
+            Map<String, Long> memo = new HashMap<>();
+            res1 += calculate(s, damaged, 0, 0, memo);
+
+            String[] d2 = n2.split(",");
+            int[] damaged2 = new int[d2.length];
+            for (int i = 0; i < d2.length; i++) damaged2[i] = Integer.parseInt(d2[i]);
+            res2 += calculate(s2, damaged2, 0, 0, memo);
+
             sc.close();
+            
         }
 
-        System.out.println(res);
+        System.out.println(res1);
+        System.out.println(res2);
     }
 
-    private long permute(String s, String n, long counter) {
-        if (!s.contains("?")) {
-            if (isCorrect(s, n)) return counter+1;
-            return counter;
-        } 
-
-        String s0 = s.replaceFirst("\\?", "#");
-        String s1 = s.replaceFirst("\\?", ".");
-        if (isNowCorrect(s0, n)) counter = permute(s0, n, counter);
-        if (isNowCorrect(s1, n)) counter = permute(s1, n, counter);
-
-        return counter;
-    }
-
-    private boolean isNowCorrect(String s, String n) {
-        String[] numbers = n.split(",");
-        int idx = 0;
-        int counter = 0;
-        for (Character c : s.toCharArray()) {
-            if (c == '?') return true;
-            if (c == '#') counter++;
-            else {
-                if (counter == 0) continue;
-                if (String.valueOf(counter).equals(numbers[idx])) idx++;
-                else return false;
-
-                if (idx == numbers.length) return true;
-                counter = 0;
-            } 
+    private long calculate(String s, int[] damaged, int len, int i, Map<String, Long> memo) {
+        if (s.length() == 0) {
+            if (len == 0) return (damaged.length == i) ? 1 : 0;
+            else return (damaged.length == i + 1 && len == damaged[i]) ? 1 : 0;
         }
-        if (String.valueOf(counter).equals(numbers[idx])) return true;
-        return false;
-    }
+        int length = s.length();
+        if (memo.containsKey(length+"-"+i+"-"+len)) return memo.get(length+"-"+i+"-"+len);
 
-    private boolean isCorrect(String s, String n) {
-        int counter = 0;
-        StringJoiner sj = new StringJoiner(",");
-        for (Character c : s.toCharArray()) {
-            if (c == '.') {
-                if (counter != 0) sj.add(String.valueOf(counter));
-                counter = 0;
-            } else counter++;
+        long res = 0;
+        List<Character> options = new ArrayList<>();
+        if (s.charAt(0) == '?') options.addAll(List.of('#', '.'));
+        else options.add(s.charAt(0));
+        
+        for (Character c : options) {
+            if (c == '#') {
+                res += calculate(s.substring(1), damaged, len+1, i, memo);
+            } else {
+                if (len > 0) {
+                    if (i < damaged.length && damaged[i] == len) res += calculate(s.substring(1), damaged, 0, i+1, memo);
+                } else {
+                    res += calculate(s.substring(1), damaged, 0, i, memo);
+                }
+            }
         }
-        if (counter > 0) sj.add(String.valueOf(counter));
-        if (sj.toString().equals(n)) return true;
-        return false;
+
+        
+        memo.put(length+"-"+i+"-"+len, res);
+        return res;
     }
 }
